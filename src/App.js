9696d6a59520d9793aa2e770776016daea9f78a7
trang <trang28092001@mail.com>
2023-02-10 14:26:41 +0700

    fix: co loi trong html

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,15 +5,17 @@ function App() {
     <div className="container d-flex align-items-center text-center">
     <div className="form-signin">
       <form>
-        <img className="mb-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png" alt="" width="72" height="57" />
-        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+        <img className="mb-4" src="https://upload.wikimedia.org/wikipedia/vi/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png" alt="" width="72" height="57" />
+        <h1 className="h3 mb-3 fw-normal">Sign in</h1>
         <div className="form-floating">
+          <label>Email address: </label>
+          <br >
           <input type="email" className="form-control email" id="floatingInput" placeholder="name@example.com" />
-          <label>Email address</label>
         </div>
         <div className="form-floating">
+          <label>Password:</label>
+          <br >
           <input type="password" className="form-control password" id="floatingPassword" placeholder="Password" />
-          <label>Password</label>
         </div>
         <div className="checkbox mb-3">
           <label>
@@ -22,7 +23,6 @@ function App() {
           </label>
         </div>
         <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        <p className="mt-5 mb-3 text-muted">2023</p>
       </form>
     </div>
   </div>
